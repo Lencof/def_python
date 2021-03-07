7cc301bd69c5a9d8da0e9a8a0cfa8a67c7ca4120
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-07 06:21:03 +0300

    Create def_Que.py

---
+++ def_Que.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# def_Qe.py
+
+def Que(): # crate def Que():
+    pass # an empty block
+  
+  A = Que # assignment
+  A = 100 # your sigures
+  B = 100 # your figures
+  print(A + B) # function print() deduces
