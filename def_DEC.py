4e91c837dee5006cee1fb2c0eb98ff7ebd8d709d
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-13 11:45:40 +0300

    Create def_DEC.py

---
+++ def_DEC.py
@@ -0,0 +1,17 @@
+# __Author__ __Lencof__ 
+# def_DEC.py
+
+def DEC(): # create def DEC():
+    print('Hi! DEC') # your text 
+    print('Hi! DEC') # your text
+    print('Hi! DEC') # your text
+    
+    print('Hi! DEC') # your text
+    print('Hi! DEC') # your text
+    print('Hi! DEC') # your text
+    
+    print('Hi! DEC') # your text
+    print('Hi! DEC') # your text
+    print('Hi! DEC') # your text
+    
+DEC() # close def DEC():
