19c9be1cd42128a66030f63e636eb3e452fe4f17
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-16 02:51:09 +0300

    Create def_Asoc.py

---
+++ def_Asoc.py
@@ -0,0 +1,11 @@
+# __Author__ __Lencof__
+# def_Asoc.py
+
+def Asoc(): # create def Asoc():
+    pass # an empty block
+  
+X = Asoc # appropriation
+X = 1000 #your figures
+V = 1000 # your figures
+
+print(X + V) # use function print()
