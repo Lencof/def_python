8969d6029335d241d2b049243d4cd37048b11e6e
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-16 02:54:37 +0300

    Create def_AD.py

---
+++ def_AD.py
@@ -0,0 +1,12 @@
+# __Author__ __Lencof__
+# def_AD.py
+
+def AD(): # create def AD():
+    pass # an empty block
+  
+X = Ad # appropriation
+X = 7 # your figures
+C = 1000 # your figures
+print(X + C) # use function print()
+
+AD() # close
