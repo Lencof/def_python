5760a1023311b9c2a492aff0516d1966f6834419
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-17 02:20:10 +0300

    Create def_Vab.py

---
+++ def_Vab.py
@@ -0,0 +1,17 @@
+# __Author__ __Lencof__
+# def_Vab.py
+
+def Vab(): # create def Vab():
+    print('Vab') # your text
+    print('Vab') # your text
+    print('Vab') # your text
+    
+    print('Vab') # your text
+    print('Vab') # your text
+    print('Vab') # your text
+    
+    print('Vab') # your text
+    print('Vab') # your text
+    print('Vab') # your text
+    
+Vab() # close
