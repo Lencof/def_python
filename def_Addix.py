62f8d276d50c5b03d748911fd58113589cb11555
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-19 02:57:48 +0300

    Create def_Addix.py

---
+++ def_Addix.py
@@ -0,0 +1,17 @@
+# __Author__ __Lencof__
+# def_Addix.py
+
+def Addix(): # create def Addix():
+    print('Addix') # your text
+    print('Addix') # your text
+    print('Addix') # your text
+    
+    print('Addix') # your text
+    print('Addix') # your text
+    print('Addix') # your text
+     
+    print('Addix') # your text
+    print('Addix') # your text
+    print('Addix') # your text
+    
+Addix() # Close()
