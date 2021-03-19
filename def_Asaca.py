8ccf1ae728b422f166aee7d21b9dabd1fa84ec26
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-19 03:13:06 +0300

    Create def_Asaca.py

---
+++ def_Asaca.py
@@ -0,0 +1,15 @@
+# __Author__ __Lencof__
+# def_Asaca.py
+
+def Asaca_a(): # create def Asaca_a():
+    global a # create global variable
+    a = 1 # your figures
+    b = 2 # your figures
+    return a + b # use function return
+  
+def Asaca_b(): # create def Asaca_b():
+    c = 3 # your figures
+    return a + c # use function return
+  
+print(Asaca_a()) # use function print()
+print(Asaca_b()) # use function print()
