a906e87218e2f2108b40f4473a8dfb9841e94f31
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-19 03:17:30 +0300

    Create def_Vavex.py

---
+++ def_Vavex.py
@@ -0,0 +1,15 @@
+# __Author__ __Lencof__
+# def_Vavex.py
+
+def Vavex_a():
+    global As
+    As = 1000
+    Do = 2000
+    return As + Do
+
+def Vavex_b():
+    Fa = 3000
+    return As + Fa
+
+print(Vavex_a())
+print(Vavex_b())
