793648d5e5ca3bac3fa0b95e0b7d98e1b721a1e6
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-21 18:13:20 +0600

    Create def_Assing.py

---
+++ def_Assing.py
@@ -0,0 +1,12 @@
+# __Author__ __Lencof__
+# def_Assing.py
+
+def Assing(): # create def Assing
+  print('Assing') # your text
+  print('Assing') # your text
+  print('Assing') # your text
+  print("Assing") # your text
+  print("Assing") # your text
+  print("Assing") # your text
+  
+Assing() # close Assing()
