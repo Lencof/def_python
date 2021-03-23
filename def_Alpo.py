a0306ec104eb6b7570349ce6492d6f9944afa5ed
Damir <71154457+Lencof@users.noreply.github.com>
2021-03-23 04:28:05 +0300

    Create def_Alpo.py

---
+++ def_Alpo.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# def_Alpo.py
+
+# create def Alpo(req1, req2, args*):
+>>>def Alpo(req1, req2, args*): # your text
+    print('Need this one:', req1) # your text
+    print('Need this one too:', req2) # your text
+    print('All the rest:', args) # your text
+    
+>>>Alpo('cap', 'gloves', 'scarf', 'monocle', 'mustache wax') # your text
