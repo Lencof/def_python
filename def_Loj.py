98070b3dc82c2c79d559f82a833c743f5aa34e45
Damir <71154457+Lencof@users.noreply.github.com>
2021-03-23 04:43:29 +0300

    Create def_Loj.py

---
+++ def_Loj.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# def_Loj.py
+
+# create def Loj(Retis1, Retis2, *args):
+def Loj(Retis1, Retis2, *args):
+    print('Hi!:', Retis1) # yuor text
+    print('Hi!:', Retis2) # your text
+    print('Hi!:', args) # your text
+    
+>>>Loj('Alex', 'Angry', 'Aliso') # the name of your friends
