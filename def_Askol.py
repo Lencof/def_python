c5e831b0f0dff56b76a4089afef3e2b19b6a568f
Damir <71154457+Lencof@users.noreply.github.com>
2021-03-23 04:49:27 +0300

    Create def_Askol.py

---
+++ def_Askol.py
@@ -0,0 +1,10 @@
+# __Author__ __Lencof__
+# def_Askol.py
+
+# create def Askol(Name1, Name2, *args):
+def Askol(Name1, Name2, *args):
+    print('Hi!:', Name1) # your text
+    print('Hi!:', Name2) # your text
+    print('Hi!:', args) # your text
+    
+>>>Askol('Aleso', 'Ana', 'Daria') # the name of your friends
