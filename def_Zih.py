2dfa71a92266a736b086ba7589dd18bcd133ce78
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-28 07:46:50 +0300

    Create def_Zih.py

---
+++ def_Zih.py
@@ -0,0 +1,41 @@
+# __Author__ __Lencof__
+# def_Zih.py
+
+def Zih(): # create class Zih()
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    print('Hello Zih') # your text
+    
+    Zih() # Close
