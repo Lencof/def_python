637ff57eac414b2c5a3a5bf74acc330443f7778f
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-28 07:44:31 +0300

    Create def_Joe.py

---
+++ def_Joe.py
@@ -0,0 +1,41 @@
+# __Author__ __Lencof__
+# def_Joe.py
+
+def Joe(): # create def Joe()
+    print('Joe') # your text
+    print('Joe') # your text
+    print('Joe') # your text
+    
+    print('Joe') # your text
+    print('Joe') # your text
+    print('Joe') # your text
+    
+    print('Joe') # your text
+    print('Joe') # your text
+    print('Joe') # your text
+    
+    print('Joe') # your text
+    print('Joe') # your text
+    print('Joe') # your text
+    
+    print('Joe') # your text
+    print('Joe') # your text
+    print('Joe') # your text
+    
+    print('Joe') # your text
+    print('Joe') # your text
+    print('Joe') # your text
+    
+    print('Joe') # your text
+    print('Joe') # your text
+    print('Joe') # your text
+    
+    print('Joe') # your text
+    print('Joe') # your text
+    print('Joe') # your text
+    
+    print('Joe') # your text
+    print('Joe') # your text
+    print('Joe') # your text
+    
+    Joe() # Close
