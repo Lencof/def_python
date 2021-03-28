2eb07c88d01e7ec8d77a1e8dbbd609eae35a1059
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-28 07:48:58 +0300

    Create def_Master.py

---
+++ def_Master.py
@@ -0,0 +1,41 @@
+# __Author__ __Lencof__
+# def_Master.py
+
+def Master(): # create class Master():
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    print('Hi! Master') # your text
+    
+    Master() # Close
