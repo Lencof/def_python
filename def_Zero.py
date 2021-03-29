dd711de9daf14ac2d6f9ff8485b0aea3d1990579
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-29 07:48:01 +0300

    Create def_Zero.py

---
+++ def_Zero.py
@@ -0,0 +1,41 @@
+# __Author__ __Lencof__
+# def_Zero.py
+
+def Zero(): # create def Zero():
+   print('Zero') # your text
+   print('Zero') # your text
+   print('Zero') # your text
+    
+   print('Zero') # your text
+   print('Zero') # your text
+   print('Zero') # your text
+  
+   print('Zero') # your text
+   print('Zero') # your text
+   print('Zero') # your text
+    
+   print('Zero') # your text
+   print('Zero') # your text
+   print('Zero') # your text
+    
+   print('Zero') # your text
+   print('Zero') # your text
+   print('Zero') # your text
+  
+   print('Zero') # your text
+   print('Zero') # your text
+   print('Zero') # your text 
+    
+   print('Zero') # your text
+   print('Zero') # your text
+   print('Zero') # your text
+    
+   print('Zero') # your text
+   print('Zero') # your text
+   print('Zero') # your text
+  
+   print('Zero') # your text
+   print('Zero') # your text
+   print('Zero') # your text
+    
+   Zero() # Close
