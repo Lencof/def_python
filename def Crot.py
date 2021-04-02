a912de49214e8c9f52819b3a2acb49fbf1879de1
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-02 14:42:06 +0300

    Create def Crot.py

---
+++ def Crot.py
@@ -0,0 +1,42 @@
+# __Author__ __Lencof__
+# def Crot.py
+
+# create def Crot():
+def Crot():
+    print('Crot') # your text
+    print('Crot') # your text
+    print('Crot') # your text
+    
+    print('Crot') # your text
+    print('Crot') # your text
+    print('Crot') # your text
+    
+    print('Crot') # your text
+    print('Crot') # your text
+    print('Crot') # your text
+    
+    print('Crot') # your text
+    print('Crot') # your text
+    print('Crot') # your text
+    
+    print('Crot') # your text
+    print('Crot') # your text
+    print('Crot') # your text
+    
+    print('Crot') # your text
+    print('Crot') # your text
+    print('Crot') # your text
+    
+    print('Crot') # your text
+    print('Crot') # your text
+    print('Crot') # your text
+    
+    print('Crot') # your text
+    print('Crot') # your text
+    print('Crot') # your text
+    
+    print('Crot') # your text
+    print('Crot') # your text
+    print('Crot') # your text
+    
+Crot() # Close
