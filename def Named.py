115728b5bff976470ce92205cfba91f0f77682d1
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-03 04:45:07 +0300

    Create def Named.py

---
+++ def Named.py
@@ -0,0 +1,21 @@
+# __Author__ __Lencof__
+# def Named.py
+
+import os # use os
+
+# create def Named():
+def Named():
+    short_list = [1, 2, 3] # your figures
+while True:
+    value = input('Position [q to quit?] ') # your text
+    if value == 'q':
+        break
+    try:
+        position = int(value)
+        print(short_list[position])
+    except IndexError as err:
+        print('Bad index:', position) # your text
+    except Exception as other:
+        print('Something else broke:', other) # your text
+
+Named() # Close
