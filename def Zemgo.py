877f822c06bc1e0bb1cbc3b4f14f5e56e411b022
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-03 04:48:33 +0300

    Create def Zemgo.py

---
+++ def Zemgo.py
@@ -0,0 +1,21 @@
+# __Author__ __Lencof__
+# def Zemgo.py
+
+import os # use os 
+
+# create def Zemgo():
+def Zemgo():
+    short_list = [1, 2, 3] # your text
+while True:
+    value = input('Position [Lencof to quit?] ') # your text
+    if value == 'Lencof': # your text
+        break
+    try:
+        position = int(value)
+        print(short_list[position])
+    except IndexError as err:
+        print('Bad index:', position) # your text
+    except Exception as other:
+        print('Something else broke:', other) # your text
+
+Zemgo() # Close
