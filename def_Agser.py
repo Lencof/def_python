3997a31ce822156cff8d217d48a6bb3c0b6a03e6
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-27 11:49:53 +0300

    Create def_Agser.py

---
+++ def_Agser.py
@@ -0,0 +1,42 @@
+# __Author__ __Lencof__
+# def_Agser.py
+
+# create def Agser():
+def Agser():
+    print('Agser') # your text
+    print('Agser') # your text
+    print('Agser') # your text
+    
+    print('Agser') # your text
+    print('Agser') # your text
+    print('Agser') # your text
+    
+    print('Agser') # your text
+    print('Agser') # your text
+    print('Agser') # your text
+    
+    print('Agser') # your text
+    print('Agser') # your text
+    print('Agser') # your text
+    
+    print('Agser') # your text
+    print('Agser') # your text
+    print('Agser') # your text
+    
+    print('Agser') # your text
+    print('Agser') # your text
+    print('Agser') # your text
+    
+    print('Agser') # your text
+    print('Agser') # your text
+    print('Agser') # your text
+    
+    print('Agser') # your text
+    print('Agser') # your text
+    print('Agser') # your text
+    
+    print('Agser') # your text
+    print('Agser') # your text
+    print('Agser') # your text
+    
+Agser() # close
