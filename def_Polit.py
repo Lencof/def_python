ea1a64dee85a519a677d73f435eadee0acd9137e
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-27 11:52:01 +0300

    Create def_Polit.py

---
+++ def_Polit.py
@@ -0,0 +1,42 @@
+# __Author__ __Lencof__
+# def_Polit.py
+
+# create def Polit():
+def Polit():
+    print('Polit') # your text
+    print('Polit') # your text
+    print('Polit') # your text
+    
+    print('Polit') # your text
+    print('Polit') # your text
+    print('Polit') # your text
+    
+    print('Polit') # your text
+    print('Polit') # your text
+    print('Polit') # your text
+    
+    print('Polit') # your text
+    print('Polit') # your text
+    print('Polit') # your text
+    
+    print('Polit') # your text
+    print('Polit') # your text
+    print('Polit') # your text
+    
+    print('Polit') # your text
+    print('Polit') # your text
+    print('Polit') # your text
+    
+    print('Polit') # your text
+    print('Polit') # your text
+    print('Polit') # your text
+    
+    print('Polit') # your text
+    print('Polit') # your text
+    print('Polit') # your text
+    
+    print('Polit') # your text
+    print('Polit') # your text
+    print('Polit') # your text
+    
+Polit() # close
