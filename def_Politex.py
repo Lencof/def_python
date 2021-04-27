040846126ebbb20722e627ff2172dc4eb3b0c95e
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-27 11:54:26 +0300

    Create def_Politex.py

---
+++ def_Politex.py
@@ -0,0 +1,42 @@
+# __Author__ __Lencof__
+# def_Politex.py
+
+# create def Politex():
+def Politex():
+    print('Politex') # your text
+    print('Politex') # your text
+    print('Politex') # your text
+    
+    print('Politex') # your text
+    print('Politex') # your text
+    print('Politex') # your text
+    
+    print('Politex') # your text
+    print('Politex') # your text
+    print('Politex') # your text
+    
+    print('Politex') # your text
+    print('Politex') # your text
+    print('Politex') # your text
+    
+    print('Politex') # your text
+    print('Politex') # your text
+    print('Politex') # your text
+    
+    print('Politex') # your text
+    print('Politex') # your text
+    print('Politex') # your text
+    
+    print('Politex') # your text
+    print('Politex') # your text
+    print('Politex') # your text
+    
+    print('Politex') # your text
+    print('Politex') # your text
+    print('Politex') # your text
+    
+    print('Politex') # your text
+    print('Politex') # your text
+    print('Politex') # your text
+    
+Politex() # close
