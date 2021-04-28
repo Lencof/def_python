cea9a8eb139d96fe26fbf1913a34fb7f392380c9
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-28 03:09:30 +0300

    Create def_Gsato.py

---
+++ def_Gsato.py
@@ -0,0 +1,45 @@
+# __Author__ __Lencof__
+# def_Gsato.py
+
+import os
+import sys 
+
+# create def Gsato
+def Gsato():
+    print('Gsato') # your text
+    print('Gsato') # your text
+    print('Gsato') # your text
+    
+    print('Gsato') # your text
+    print('Gsato') # your text
+    print('Gsato') # your text
+    
+    print('Gsato') # your text
+    print('Gsato') # your text
+    print('Gsato') # your text
+    
+    print('Gsato') # your text
+    print('Gsato') # your text
+    print('Gsato') # your text
+    
+    print('Gsato') # your text
+    print('Gsato') # your text
+    print('Gsato') # your text
+    
+    print('Gsato') # your text
+    print('Gsato') # your text
+    print('Gsato') # your text
+    
+    print('Gsato') # your text
+    print('Gsato') # your text
+    print('Gsato') # your text
+    
+    print('Gsato') # your text
+    print('Gsato') # your text
+    print('Gsato') # your text
+    
+    print('Gsato') # your text
+    print('Gsato') # your text
+    print('Gsato') # your text
+    
+Gsato() # close
