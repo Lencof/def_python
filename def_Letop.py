a287e1423a7199852bafae9073fbc799da199dfa
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-28 03:12:13 +0300

    Create def_Letop.py

---
+++ def_Letop.py
@@ -0,0 +1,45 @@
+# __Author__ __Lencof__
+# def_Letop.py
+
+import os
+import sys
+
+# create def Letop
+def Letop():
+    print('Letop') # your text
+    print('Letop') # your text
+    print('Letop') # your text
+    
+    print('Letop') # your text
+    print('Letop') # your text
+    print('Letop') # your text
+    
+    print('Letop') # your text
+    print('Letop') # your text
+    print('Letop') # your text
+    
+    print('Letop') # your text
+    print('Letop') # your text
+    print('Letop') # your text
+    
+    print('Letop') # your text
+    print('Letop') # your text
+    print('Letop') # your text
+    
+    print('Letop') # your text
+    print('Letop') # your text
+    print('Letop') # your text
+    
+    print('Letop') # your text
+    print('Letop') # your text
+    print('Letop') # your text
+    
+    print('Letop') # your text
+    print('Letop') # your text
+    print('Letop') # your text
+    
+    print('Letop') # your text
+    print('Letop') # your text
+    print('Letop') # your text
+    
+Letop() # close
