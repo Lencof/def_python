0a98784fe8a73aefd2522a3189cd0fa0351527a6
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-28 03:06:48 +0300

    Create def_Xep.py

---
+++ def_Xep.py
@@ -0,0 +1,45 @@
+# __Author__ __Lencof__
+# def_Xep.py
+
+import os
+import sys
+
+# create def Xep
+def Xep():
+    print('Xep') # your text
+    print('Xep') # your text
+    print('Xep') # your text
+    
+    print('Xep') # your text
+    print('Xep') # your text
+    print('Xep') # your text
+    
+    print('Xep') # your text
+    print('Xep') # your text
+    print('Xep') # your text
+    
+    print('Xep') # your text
+    print('Xep') # your text
+    print('Xep') # your text
+    
+    print('Xep') # your text
+    print('Xep') # your text
+    print('Xep') # your text
+    
+    print('Xep') # your text
+    print('Xep') # your text
+    print('Xep') # your text
+    
+    print('Xep') # your text
+    print('Xep') # your text
+    print('Xep') # your text
+    
+    print('Xep') # your text
+    print('Xep') # your text
+    print('Xep') # your text
+    
+    print('Xep') # your text
+    print('Xep') # your text
+    print('Xep') # your text
+    
+Xep() # close
