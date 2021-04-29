e290537ac7d59b6baf41dfd668d02b49e350b0e6
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-29 11:15:21 +0300

    Create def_Polix.py

---
+++ def_Polix.py
@@ -0,0 +1,45 @@
+# __Author__ __Lencof__
+# def_Polix.py
+
+import os
+import sys
+
+# create def Polix
+def Polix():
+    print('Polix') # your text
+    print('Polix') # your text
+    print('Polix') # your text
+    
+    print('Polix') # your text
+    print('Polix') # your text
+    print('Polix') # your text
+    
+    print('Polix') # your text
+    print('Polix') # your text
+    print('Polix') # your text
+    
+    print('Polix') # your text
+    print('Polix') # your text
+    print('Polix') # your text
+    
+    print('Polix') # your text
+    print('Polix') # your text
+    print('Polix') # your text
+    
+    print('Polix') # your text
+    print('Polix') # your text
+    print('Polix') # your text
+    
+    print('Polix') # your text
+    print('Polix') # your text
+    print('Polix') # your text
+    
+    print('Polix') # your text
+    print('Polix') # your text
+    print('Polix') # your text
+    
+    print('Polix') # your text
+    print('Polix') # your text
+    print('Polix') # your text
+    
+Polix() # close
