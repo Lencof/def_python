4e477214ee0cf2f405a1812291cbdc4c561a1949
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-29 11:18:02 +0300

    Create def_Leto.py

---
+++ def_Leto.py
@@ -0,0 +1,45 @@
+# __Author__ __Lencof__
+# def_Leto.py
+
+import os
+import sys
+
+# create def Leto
+def Leto():
+    print('Leto') # your text
+    print('Leto') # your text
+    print('Leto') # your text
+    
+    print('Leto') # your text
+    print('Leto') # your text
+    print('Leto') # your text
+    
+    print('Leto') # your text
+    print('Leto') # your text
+    print('Leto') # your text
+    
+    print('Leto') # your text
+    print('Leto') # your text
+    print('Leto') # your text
+    
+    print('Leto') # your text
+    print('Leto') # your text
+    print('Leto') # your text
+    
+    print('Leto') # your text
+    print('Leto') # your text
+    print('Leto') # your text
+    
+    print('Leto') # your text
+    print('Leto') # your text
+    print('Leto') # your text
+    
+    print('Leto') # your text
+    print('Leto') # your text
+    print('Leto') # your text
+    
+    print('Leto') # your text
+    print('Leto') # your text
+    print('Leto') # your text
+    
+Leto() # close
