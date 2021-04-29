0a4d7615a6d85e102ac39348627b56c945127644
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-29 11:20:32 +0300

    Create def_Letel.py

---
+++ def_Letel.py
@@ -0,0 +1,45 @@
+# __Author__ __Lencof__
+# def_Letel.py
+
+import os
+import sys
+
+# create def Letel
+def Letel():
+    print('Letel') # your text
+    print('Letel') # your text
+    print('Letel') # your text
+    
+    print('Letel') # your text
+    print('Letel') # your text
+    print('Letel') # your text
+    
+    print('Letel') # your text
+    print('Letel') # your text
+    print('Letel') # your text
+    
+    print('Letel') # your text
+    print('Letel') # your text
+    print('Letel') # your text
+    
+    print('Letel') # your text
+    print('Letel') # your text
+    print('Letel') # your text
+    
+    print('Letel') # your text
+    print('Letel') # your text
+    print('Letel') # your text
+    
+    print('Letel') # your text
+    print('Letel') # your text
+    print('Letel') # your text
+    
+    print('Letel') # your text
+    print('Letel') # your text
+    print('Letel') # your text
+    
+    print('Letel') # your text
+    print('Letel') # your text
+    print('Letel') # your text
+    
+Letel() # close
