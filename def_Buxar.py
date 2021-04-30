2b5edf4ebcf5a7c69e8c0af0a2a99b539ac4cf60
Lencof <71154457+Lencof@users.noreply.github.com>
2021-04-30 07:48:07 +0300

    Update def_Buxar.py

--- def_Buxar.py
+++ def_Buxar.py
@@ -1,6 +1,9 @@
 # __Author__ __Lencof__
 # def_Buxar.py
 
+import os
+import sys 
+
 def Buxar(): # create class Buxar():
     print('Buxar') # your text
     print('Buxar') # your text
@@ -13,5 +16,29 @@ def Buxar(): # create class Buxar():
     print('Buxar') # your text
     print('Buxar') # your text
     print('Buxar') # your text
-  
+    
+    print('Buxar') # your text
+    print('Buxar') # your text
+    print('Buxar') # your text
+    
+    print('Buxar') # your text
+    print('Buxar') # your text
+    print('Buxar') # your text
+    
+    print('Buxar') # your text
+    print('Buxar') # your text
+    print('Buxar') # your text
+    
+    print('Buxar') # your text
+    print('Buxar') # your text
+    print('Buxar') # your text
+    
+    print('Buxar') # your text
+    print('Buxar') # your text
+    print('Buxar') # your text
+    
+    print('Buxar') # your text
+    print('Buxar') # your text
+    print('Buxar') # your text
+    
 Buxar() # Close()
