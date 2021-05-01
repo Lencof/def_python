c211873ef817eaa4c2b1c31745c92cef42fa9983
Lencof <71154457+Lencof@users.noreply.github.com>
2021-05-01 10:41:59 +0300

    Update def_Master.py

--- def_Master.py
+++ def_Master.py
@@ -1,6 +1,9 @@
 # __Author__ __Lencof__
 # def_Master.py
 
+import os
+import sys 
+
 def Master(): # create class Master():
     print('Hi! Master') # your text
     print('Hi! Master') # your text
