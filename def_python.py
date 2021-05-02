f0455f69cf4d3d23a04d7269352198b9cb45a341
Lencof <71154457+Lencof@users.noreply.github.com>
2021-05-02 09:52:33 +0300

    Update def_python.py

--- def_python.py
+++ def_python.py
@@ -1,6 +1,44 @@
+# __Author__ __Lencof__
+# def_python.py
+
+import os
+import sys
+
 def Dior(): # create Dior
   print('Hello World') # your text
   print('Hello World') # your text
   print('Hello World') # your text
 
+  print('Hello World') # your text
+  print('Hello World') # your text
+  print('Hello World') # your text
+  
+  print('Hello World') # your text
+  print('Hello World') # your text
+  print('Hello World') # your text
+  
+  print('Hello World') # your text
+  print('Hello World') # your text
+  print('Hello World') # your text
+
+  print('Hello World') # your text
+  print('Hello World') # your text
+  print('Hello World') # your text
+  
+  print('Hello World') # your text
+  print('Hello World') # your text
+  print('Hello World') # your text
+  
+  print('Hello World') # your text
+  print('Hello World') # your text
+  print('Hello World') # your text
+
+  print('Hello World') # your text
+  print('Hello World') # your text
+  print('Hello World') # your text
+  
+  print('Hello World') # your text
+  print('Hello World') # your text
+  print('Hello World') # your text
+  
 Dior() # close Dior()
